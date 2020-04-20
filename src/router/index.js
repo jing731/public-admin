30db3bcb7f8a281ec781a56444f7c292beb1d8df
jing <3223191529@qq.com>
2020-04-21 07:44:14 +0800

    完成了基本样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/views/login'
+import Login from '@/views/login/'
 Vue.use(VueRouter)
 const routes = [
   {
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,76 @@
 <template>
-  <div class="login-container">登陆页面</div>
+  <div class="login-container">
+    <div class="login-form-wrap">
+      <div class="login-header"></div>
+    <el-form class="login-from" ref="form" :model="user">
+  <el-form-item>
+    <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
+  </el-form-item>
+   <el-form-item >
+    <el-input v-model="user.code"  placeholder="请输入密码"></el-input>
+  </el-form-item>
+   <el-form-item >
+   <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+  </el-form-item>
+  <el-form-item >
+    <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+  </el-form-item>
+</el-form></div>
+  </div>
 </template>
 <script>
-export default ({
+export default {
   name: 'LoginIndex',
-  component: {},
+  components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        modile: '',
+        code: ''
+      },
+      checked: false
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  metheds: {}
-})
+  methods: {
+    onSubmit () {
+      console.log('11')
+    }
+  }
+}
 </script>
 <style scoped lang='less'>
-
+.login-container{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: url("./login_bg.jpg") no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .login-form-wrap{
+    background-color: #fff;
+    padding: 50px;
+    min-width: 400px;
+   .login-header{
+    background: url("./logo_index.png");
+    height: 57px;
+    width: 400px;
+    margin-bottom: 30px;
+  }
+  .login-from{
+    .login-btn{
+      width: 100%;
+    }
+  }
+  }
+}
 </style>
