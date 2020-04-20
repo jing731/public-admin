52a70b0dca094d6e01a0e1684ae69cf40eb937f3
jing <3223191529@qq.com>
2020-04-20 21:01:44 +0800

    创建组件并配置路由

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-container">登陆页面</div>
+</template>
+<script>
+export default ({
+  name: 'LoginIndex',
+  component: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  metheds: {}
+})
+</script>
+<style scoped lang='less'>
+
+</style>
