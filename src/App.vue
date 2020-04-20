78f484671fab13089106eee7d63b7346745adcb8
jing <3223191529@qq.com>
2020-04-20 18:30:10 +0800

    完成了基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
-    <h1>内容发布系统</h1>
-    <h2>uuu</h2>
-    <h3>的伙食费离开</h3>
+  <h1>内容</h1>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>>
+<style lang="less">
+</style>
